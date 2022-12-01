3cfffb5ae7e2f735bfb239abc4e9f9739361eafc
unknown <jacarmonamartin@gmail.com>
2022-12-01 20:15:22 +0100

    logic kinda working

--- pierre-groceries/src/components/item.tsx
+++ pierre-groceries/src/components/item.tsx
@@ -1,10 +1,30 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { ItemChild } from "../services/types";
 
 const Item = ({ grocery, key, index, deleteGrocery }: ItemChild) => {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            {grocery}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={() => deleteGrocery(index)}>
+            Delete item
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
 };
 
 export default Item;
