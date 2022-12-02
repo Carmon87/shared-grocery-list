f31180a8d063d312558e42f80538fe4a903eac69
unknown <jacarmonamartin@gmail.com>
2022-12-02 15:37:10 +0100

    add grocery button logic

--- pierre-groceries/src/App.tsx
+++ pierre-groceries/src/App.tsx
@@ -4,7 +4,6 @@ import ItemList from './components/ItemList'
 function App() {
   return (
     <div className="App">
-     <h1>Groceries go here somewhere</h1>
     <ItemList/>
     </div>
   );
--- pierre-groceries/src/components/inputGrocery.tsx
+++ pierre-groceries/src/components/inputGrocery.tsx
@@ -2,11 +2,15 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
-import {InputGroceryChild} from '../services/types'
-
+import { InputGroceryChild } from "../services/types";
 
 const InputGrocery = ({ setItemList, pendingItemList }: InputGroceryChild) => {
   const [groceryItem, setGroceryItem] = React.useState("");
+  const [toggleButton, setToggleButton] = React.useState(true);
+
+  React.useEffect(() => {
+    groceryItem.length ? setToggleButton(false) : setToggleButton(true)
+  }, [groceryItem]);
 
   return (
     <div>
@@ -23,15 +27,19 @@ const InputGrocery = ({ setItemList, pendingItemList }: InputGroceryChild) => {
           label="Item"
           variant="outlined"
           placeholder="Add your grocery item"
+          value={groceryItem}
           onChange={(event) => {
             setGroceryItem(event.target.value);
           }}
         />
+
         <Button
           variant="outlined"
+          disabled={toggleButton}
           onClick={(event: React.SyntheticEvent) => {
             event.preventDefault();
-            setItemList([...pendingItemList, groceryItem])
+            setItemList([...pendingItemList, groceryItem]);
+            setGroceryItem("");
           }}
         >
           Add to list
